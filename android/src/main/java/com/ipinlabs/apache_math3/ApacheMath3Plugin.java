5ace4525a18f0ec3a670cda2c7e92923fa42de05
gwanggyuchoi <gwanggyu.choi@ipinlabs.com>
2023-07-06 14:33:42 +0900

    프린트문 삭제

--- android/src/main/java/com/ipinlabs/apache_math3/ApacheMath3Plugin.java
+++ android/src/main/java/com/ipinlabs/apache_math3/ApacheMath3Plugin.java
@@ -39,21 +39,19 @@ public class ApacheMath3Plugin implements FlutterPlugin, MethodCallHandler {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     }else if(call.method.equals("linearErp")) {
       try {
-        List<Integer> t_in = call.argument("input");
-//      List<Double> originValue = call.argument("value");
-        List<Integer> originOutput = call.argument("output");
-
+        List<Integer> t_in = call.argument("input"); // 다트의 시간은 안먹어
         List<Long> longTinList = new ArrayList<>();
         for (Integer inElement : t_in) {
           longTinList.add(inElement.longValue());
         }
+
         List<Double> originValue = call.argument("value");
-        List<Double> tempValueList = new ArrayList<>(originValue);
         List<Float> floatValue = new ArrayList<>();
         for (Double element : originValue) {
           floatValue.add(element.floatValue());
         }
 
+        List<Integer> originOutput = call.argument("output");
         long[] t_out = new long[originOutput.size()];
         for (int i = 0; i < originOutput.size(); i++) {
           t_out[i] = originOutput.get(i).longValue();
@@ -67,10 +65,7 @@ public class ApacheMath3Plugin implements FlutterPlugin, MethodCallHandler {
         double[] temp = Arrays.stream(t_out).mapToDouble(psf::value).toArray();
         result.success(temp);
 
-        System.out.println("11: success" + temp.length);
-
       } catch (Error e) {
-        System.out.println("e:" + e.getMessage());
         result.error("error", e.getMessage(),null);
       }
     }else {
